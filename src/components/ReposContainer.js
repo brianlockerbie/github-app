3601f69a92046101052d23e43632d61b00ba1d3f
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-10-12 11:23:50 -0400

    Created Repos and imported it into ReposContainer

--- src/components/ReposContainer.js
+++ src/components/ReposContainer.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Overview from "./Overview";
+import Repos from "./Repos";
 
 const ReposContainer = () => {
   return (
     <div className="reposContainer">
       <Route path="/overview" component={Overview} />
-      <Route path="/repos" />
+      <Route path="/repos" component={Repos} />
       <Route path="/followers" />
     </div>
   );
